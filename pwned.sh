beef511f3e03e333bae36a9875f86e5e0d648bfa
jwrichards <jwrichards@gmail.com>
2017-05-16 10:51:34 -0700

    Improved jq using the map(. +{key:value}) format to add email to results before throwing in DB.

--- pwned.sh
+++ pwned.sh
@@ -1,18 +1,22 @@
 #!/bash/bin
 
-rm /Users/jrichards/Desktop/PwnChecker/beenpwned.json
-if [ -z "$1" ]
+rm /Users/jrichards/Desktop/PwnChecker/raw.json
+rm /Users/jrichards/Desktop/PwnChecker/formatted.json
+rm /Users/jrichards/Desktop/PwnChecker/upload.json
+if [ -z "$1" ] #check for argument
 then
-  echo "Potentially pwned positronic mail:"
-  read EMAILNAME
-else
-  EMAILNAME="$1"
+  echo 'Requires email argument. Format pwned <email>'
+  exit 1 #terminate and indicate error if no email address found
 fi
+  EMAILNAME="$1"
 
-cat /Users/jrichards/Desktop/PwnChecker/header > /Users/jrichards/Desktop/PwnChecker/beenpwned.json
-curl 'https://haveibeenpwned.com/api/v2/breachedaccount/'"$EMAILNAME" | jq '.' >> /Users/jrichards/Desktop/PwnChecker/beenpwned.json
+cat /Users/jrichards/Desktop/PwnChecker/header > /Users/jrichards/Desktop/PwnChecker/upload.json
+curl 'https://haveibeenpwned.com/api/v2/breachedaccount/'"$EMAILNAME" | jq '.' > /Users/jrichards/Desktop/PwnChecker/raw.json
+#cat /Users/jrichards/Desktop/PwnChecker/beenpwned.json >> /Users/jrichards/Desktop/PwnChecker/formatted.json
 echo "Pwned email:"
 echo $EMAILNAME
-jq -r '.[].Name' /Users/jrichards/Desktop/PwnChecker/beenpwned.json
-sed -i '$a }' /Users/jrichards/Desktop/PwnChecker/beenpwned.json
-curl -H "Content-Type: application/json" -d @$PWD/beenpwned.json -X POST http://jrichards:Mycouchisafuton.@127.0.0.1:5984/testpwn/_bulk_docs
+jq -r '.[].Name' /Users/jrichards/Desktop/PwnChecker/raw.json
+jq  --arg ENAME $EMAILNAME 'map(. + {Email: $ENAME})' /Users/jrichards/Desktop/PwnChecker/raw.json >> /Users/jrichards/Desktop/PwnChecker/formatted.json
+cat /Users/jrichards/Desktop/PwnChecker/formatted.json >> /Users/jrichards/Desktop/PwnChecker/upload.json
+sed -i '$a }' /Users/jrichards/Desktop/PwnChecker/upload.json
+curl -H "Content-Type: application/json" -d @$PWD/upload.json -X POST http://jrichards:Mycouchisafuton.@127.0.0.1:5984/testpwn/_bulk_docs
